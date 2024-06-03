4e7e2a5639df66c80431645c8fa4b09e4a2e8444
NovikovIlya <novikov.i.sergeevich@gmail.com>
2024-06-03 17:22:16 +0300

    add handles

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,22 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedOption, setSelectedOption] = useState(''); 
+  const [inputValue, setInputValue] = useState(''); 
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedOption(event.target.value);
+  };
 
   return (
     <>
       <form style={{display:'flex', flexWrap:'wrap'}} className=''>
         <input  />
-        <select>
+        <select value={selectedOption} onChange={handleSelectChange}>
           <option value="">-- Выберите опцию --</option>
           <option value="user">user</option>
           <option value="repo">repo</option>
